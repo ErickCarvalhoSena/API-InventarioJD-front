1cfa76b8b78c73cc6b04d22e0774d1b28c8cca6b
Erick Sena <erick087sena@gmail.com>
2026-06-01 22:49:09 -0300

    feat: pesquisa em tempo real conectada na API

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,79 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+import { useState, useEffect } from "react";
+
+interface Peca{
+  id: number;
+  codigo: string;
+  descricao: string;
+  quantidade: number;
+  localizacao: string;
+  modelos: string[];
+}
+
+export default function Home(){
+  const [busca, setBusca] = useState("");
+  const [pecas, SetPecas] = useState<Peca[]>([]);
+
+  useEffect(() => {
+    if (busca.length === 0) {
+      SetPecas([]);
+      return;
+    }
+
+    const timer = setTimeout(async () => {
+      const res = await fetch(`http://localhost:5090/api/Pecas/${busca}`);
+      if(res.ok){
+        const data = await res.json();
+        SetPecas([data]);
+      } else {
+        SetPecas([]);
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [busca]);
+
+  return(
+      <main className="min-h-screen bg-gray-100">
+        <div className="bg-green-700 text-white text-center py-6">
+          <h1 className="text-3x1 font-bold">Oficina RGF</h1>
+          <p className="text-green-200">Controle de Estoque de Peças</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="max=w=4x1 mx-auto p-6">
+          <h2 className="text-x1 font-semibold mb-4">Pesquisar Peças</h2>
+
+          <div className="bg-white rounded-lg shadow p-4 mb-6">
+            <input
+              type="text"
+              placeholder="Digite o código da peça..."
+              className="w-full border rounded p-2"
+              value={busca}
+              onChange={(e) => setBusca(e.target.value)}
+              />
+          </div>
+           
+           {pecas.length === 0 ? (
+            <p className="text-gray-500">Nenhuma peça encontrada.</p>
+           ) : (
+            <div className="flex flex-col gap-4">
+              {pecas.map((peca) => (
+                <div key={peca.id} className="bg-white rounded-lg shadow p-4">
+                  <div className="flex justity-between items-center">
+                    <h3 className="text-lg font-bold text-green-700">{peca.codigo}</h3>
+                    <span className="text-sm bg-green-100 text-green-800 px-2 py-1 rounded">
+                      Qtd: {peca.quantidade}
+                    </span>
+                  </div>
+                  <p className="text-gray-600">{peca.descricao}</p>
+                  <p className="text-sm text-gray-400">📍  {peca.localizacao}</p>
+                  <p className="text-sm text-gray-400">🚜  {peca.modelos.join(", ")}</p>
+                  </div>
+              ))}
+              </div>
+           )}
         </div>
       </main>
-    </div>
   );
 }
